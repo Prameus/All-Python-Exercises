b12ba9195522c2b42ae29b973ab0e7ef0db0233e
Prameus <musa_karaaslan2@hotmail.com>
2021-12-07 16:51:19 +0300

    secondUpdate

--- ogrenciKaydi.py
+++ ogrenciKaydi.py
@@ -1,68 +1,80 @@
+import json
+
 studentList = {
-    "001":{
-    "Name":"Musa",
-    "Surname":"Karaaslan",
-    "Class":305,
-    "Gender":"helicopter",
-    "student Number":921,
-    "Students Grade-Point average":74
-}}
+    "001": {
+        "Name": "Musa",
+        "Surname": "Karaaslan",
+        "Class": 305,
+        "Gender": "helicopter",
+        "student Number": 921,
+        "Students Grade-Point average": 74
+    }}
 
-def studentInformations():
-    name = input(print('Please Enter Students Name'))
-    
-    surname = input(print('Please Enter Students Surname'))
-    
-    clas = input(print('Please Enter Students Class'))
-    
-    gender = input(print('Please Enter Students Gender'))
-    
-    studentNumber = input(print('Please Enter Students studentNumber'))
-    
-    gpa = input(print('Please Enter Students Grade-Point average')) 
 
-    c = 0
-    c += 1
+def writeToFile(a):
+    with open("studentInformations.json", "w") as file:
+        file.write(json.dumps(a))
 
-    studentList.update({"Name": name, "Surname": surname, "Class": clas, "Gender": gender, "Student Number": studentNumber, "Students Grade-Point average":gpa})
-   
-fieldChoice = ["name", "name", "class", "gender", "student number", "grade-point average"]
+
+def readFromFile():
+    try:
+        with open("studentInformations.json", "r") as file:
+            file.read("studentInformations.json")
+            json.loads(file.read("studentInformations.json"))
+    except:
+        pass
+
+
+fieldChoice = ["name", "surname", "class", "gender",
+               "student number", "grade-point average"]
+
 
 def editStudents():
     while True:
         print(studentList)
-        editChoice = str(input('Please Enter the ID of student that you want to change: '))
+        editChoice = input(
+            'Please Enter the ID of student that you want to change: ')
         student = studentList[editChoice]
         if editChoice in studentList:
             while True:
-                editParams = str(input('Please Write which Property You Want to Change'))
-                if editParams.lower in fieldChoice:
+                editParams = str(
+                    input('Please Write which Property You Want to Change: '))
+                if editParams in fieldChoice:
                     break
                 else:
-                    print('You Write a Property That Does not Exist, Please Write Again')
-            newValue = input(f'Enter The New Value of {editParams.lower}: ')
-            student.update({editParams:newValue})
-            studentList.update({editChoice:student})
+                    print(
+                        'You Write a Property That Does not Exist, Please Write Again')
+            newValue = input(f'Enter The New Value of {editParams}: ')
+            student.update({editParams: newValue})
+            studentList.update({editChoice: student})
+            isExit = str(
+                input('Is There Any Other Changes You Want to Do? If There is not Write `exit`'))
+            if isExit.lower == "exit":
+                break
         else:
             print('You Picked a Number That Does not Exist')
+            writeToFile(studentList)
+
 
 newStudent = {}
+
+
 def addStudents():
     newID = input('Please Specify New Students ID: ')
     if newID in studentList:
         print('This ID is Already Taken, Please Enter Some Other ID')
     for i in fieldChoice:
         value = input(f"Please Enter the {i} of New Student")
-        newStudent.update({i:value})
-        studentList.update({newID:newStudent})
-    
-
+        newStudent.update({i: value})
+        studentList.update({newID: newStudent})
+    writeToFile(studentList)
 
 
 def mainTab():
     while True:
         print(studentList)
-        choice = str(input('Choice Your Action;\n1 For Edit Students,\n2 For Add Students\n For Exit please Write "exit" \n Please Enter an Action: '))
+        choice = str(input(
+            'Choice Your Action;\n1 For Edit Students,\n2 For Add Students\n For Exit please Write "exit" \n Please Enter an Action: '))
         if choice == "1":
             editStudents()
         elif choice == "2":
@@ -73,4 +85,4 @@ def mainTab():
             pass
 
 
-mainTab() 
+mainTab()
