b9904217359bada322cc60091dc72a8e6bffc54d
musa <musa_karaaslan2@hotmail.com>
2021-12-10 23:09:14 +0300

    .

---
+++ classOgrenciKaydi.py
@@ -0,0 +1,9 @@
+class StudentList:
+    def __init__(self, name, surname, age, gender, claass, studentNumber, GPA):
+        self.name = name
+        self.surname = surname
+        self.age = age
+        self.gender = gender
+        self.claass = claass
+        self.studentNumber = studentNumber
+        self.GPA = GPA
